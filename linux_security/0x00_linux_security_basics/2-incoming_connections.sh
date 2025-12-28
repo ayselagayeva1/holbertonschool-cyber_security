fd9e28e58236edd36b321fbf107121be22513eba
ayselagayeva1 <ayselagayevaa13@gmail.com>
2025-12-28 18:18:56 +0000

    update

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,2 @@
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
