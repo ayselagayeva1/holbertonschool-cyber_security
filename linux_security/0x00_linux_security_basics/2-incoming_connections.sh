cd2a7fe0067c48a3ae92188984efcc1d69aead26
ayselagayeva1 <ayselagayevaa13@gmail.com>
2025-12-28 18:18:14 +0000

    update

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,26 +1,2 @@
-#!/bin/bash
-
-# Flush existing rules
-iptables -F
-ip6tables -F
-
-# Default policy: drop incoming traffic
-iptables -P INPUT DROP
-ip6tables -P INPUT DROP
-
-# Allow loopback
-iptables -A INPUT -i lo -j ACCEPT
-ip6tables -A INPUT -i lo -j ACCEPT
-
-# Allow established and related connections
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-ip6tables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-# Allow incoming TCP connections on port 80 (IPv4)
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-
-# Allow incoming TCP connections on port 80 (IPv6)
 ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
-
-echo "Rules updated"
-echo "Rules updated (v6)"
