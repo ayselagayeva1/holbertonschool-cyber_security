f5596fdffd99c1fd4748e05bfc07771a7c1d6046
ayselagayeva1 <ayselagayevaa13@gmail.com>
2026-02-02 02:32:27 -0500

    Update 0-sha1.sh

--- cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
+++ cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then
-  echo "Usage: $0 <password>"
-  exit 1
-fi
-
-echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
-
-echo "Password hash has been saved to 0_hash.txt."
+echo -n "$1" | sha1sum | cut -d' ' -f1 > 0_hash.txt
