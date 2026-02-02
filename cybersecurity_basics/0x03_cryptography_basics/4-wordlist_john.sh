30b23b5a61d58f0a9c0700733cab4c64325920f7
ayselagayeva1 <ayselagayevaa13@gmail.com>
2026-02-02 06:43:37 -0500

    Quick commit

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1"
-john --show "$1" | grep -v "Warning" | grep -v "No password hashes left" > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | awk -F: '{print $2}' > 4-password.txt
