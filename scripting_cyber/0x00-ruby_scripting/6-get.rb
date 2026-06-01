423651127d12d996e696ce7279ab8302b2e507ac
ayselagayeva1 <ayselagayevaa13@gmail.com>
2026-06-01 10:29:01 +0400

    Create 6-get.rb

---
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -0,0 +1,11 @@
+require 'net/http'
+require 'json'
+
+def get_request(url)
+  uri      = URI(url)
+  response = Net::HTTP.get_response(uri)
+
+  puts "Response status: #{response.code} #{response.message}"
+  puts "Response body:"
+  puts JSON.pretty_generate(JSON.parse(response.body))
+end
